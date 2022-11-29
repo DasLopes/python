aa81aea48076c01be0ce1199d4b6cfe957d6a382
daslopes <daslopes@outlook.com.br>
2022-11-29 11:39:39 -0300

    Create aula09.py
    aula 09 parcialmente

---
+++ aulas/aula09.py
@@ -0,0 +1,26 @@
+frase = '    Curso em Vídeo Python    '
+#print(frase) #mostra todo o conteúdo da string
+
+#print(frase[3]) #mostra o conteúdo da string na posição 3
+
+#print(frase[3:13]) #mostra o conteúdo da string da posição 3 até a posição 13
+
+#print(frase[:13]) #mostra a string da posição 0 até a posição 13
+
+#print(frase[13:]) #mostra o conteudo da string da posição 13 até o final
+
+#print(frase[1:15:2]) #mostra o que há na string, da posição 1 até a posição 15, pulando de 2 em 2
+
+#print('oi') #mostra um oi na tela
+
+#print('''Cedo ou tarde, você vai aprender, assim como eu aprendi, que existe uma diferença entre CONHECER o caminho e TRILHAR o caminho.(filme Matrix)''') #mostra a frase mesmo se ela tiver mais de uma linha. Bom para cabeçalho.
+
+#print(frase.count('o')) #mostra quantas vezes aparece a letra o
+
+#print(frase.upper().count('O')) #transforma todas as letras para maiusculas e mostra quantas vezes aparece a letra O maniuscula
+
+#print(len(frase)) #mostra quanto digitos tem a frase
+
+#print(len(frase.strip())) #mostra quantos digitados tem a frase desconsiderando os espaços vazios antes e depois da frase.
+
+#print(frase.replace('Python', 'Android'))  #mostra a string na tela substituindo a palavra python por android
