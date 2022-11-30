f2fcfbc600fca4d1ab3c575c31d4a2e421ccc113
daslopes <daslopes@outlook.com.br>
2022-11-30 12:00:43 -0300

    exercícios 23 a 27

---
+++ exercicios/ex024.py
@@ -0,0 +1,3 @@
+cid = str(input('Em que cidade você nasceu? ')).strip().upper()
+print('SANTO' in cid) #primeira solução
+print(cid[:5] == 'SANTO') #segunda solução
---
+++ exercicios/ex027.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome completo: ')).strip().split()
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[-1])) #primeira solução
+print('Seu último nome é {}'.format(nome[len(nome) -1 ])) #segunda solução
