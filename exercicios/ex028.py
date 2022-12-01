ec58b3b6fe03b69d54ae2fc94225ac9db5655c6b
daslopes <daslopes@outlook.com.br>
2022-12-01 12:09:11 -0300

    create ex028
    create ex028

---
+++ exercicios/ex028.py
@@ -0,0 +1,18 @@
+'''
+Escreva um programa que faça o computador "pensar" em um
+número inteiro entre 0 e 5 e peça para o usuário tentar descobrir
+qual foi o número escolhido pelo computador. O programa deverá
+escrever na tela se o usuário venceu ou perdeu.'''
+import random, time
+
+sort = random.randrange(0,5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+num = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+time.sleep(2)
+if sort == num:
+    print('PARABÉNS! Você conseguiu me vender!')
+else:
+    print('GANHEI! Eu pensei no número {} e não no {}!'.format(sort, num))
