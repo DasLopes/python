efc0dfd22a34f71da8b9a66211ea049432c15aef
daslopes <daslopes@outlook.com.br>
2022-12-02 11:12:03 -0300

    Exercício Radar eletrônico
    Escreva um programa que leia a velocidade de um carro.
    Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
    A multa vai custar R$7,00 por cada Km acima do limite.

---
+++ exercicios/ex029.py
@@ -0,0 +1,12 @@
+''' Exercício: Radar eletrônico
+
+Escreva um programa que leia a velocidade de um carro.
+Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
+A multa vai custar R$7,00 por cada Km acima do limite.'''
+
+vel = float(input('Digite sua velocidade atual: '))
+if vel > 80:
+    multa = (vel - 80) * 7
+    print('MULTADO! Você excedeu o limite permitido que é de 80Km/h')
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+print('Tenha um bom dia! Dirija com segurança!')
