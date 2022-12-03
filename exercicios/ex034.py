45b283fee18a3c0bc5c082b61130be2f71fce129
daslopes <daslopes@outlook.com.br>
2022-12-03 09:59:45 -0300

    Exercício porcentagem
    Escreva um programa que pergunte o salário de um funcionário e
    calcule o valor do seu aumento.
    Para salários superiores a R$1.250,00, calcule um aumento de 10%.
    Para os inferiores ou iguais, o aumento é de 15%.

---
+++ exercicios/ex034.py
@@ -0,0 +1,13 @@
+'''
+Escreva um programa que pergunte o salário de um funcionário e
+calcule o valor do seu aumento.
+Para salários superiores a R$1.250,00, calcule um aumento de 10%.
+Para os inferiores ou iguais, o aumento é de 15%.
+'''
+
+salario = float(input('Qual é o salário do funcionário? R$'))
+if salario > 1250:
+    novo = salario + (salario / 100 * 10)
+else:
+    novo = salario + (salario / 100 * 15)
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salario, novo))
