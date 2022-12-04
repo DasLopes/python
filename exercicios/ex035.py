456beecab5ac1cc398cf38b3f9ffeb0d0397e88c
daslopes <daslopes@outlook.com.br>
2022-12-04 18:20:36 -0300

    Exercício triângulo
    Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.

---
+++ exercicios/ex035.py
@@ -0,0 +1,17 @@
+'''
+Desenvolva um programa que leia o comprimento
+de três retas e diga ao usuário se elas podem ou
+não formar um triângulo.
+'''
+print('=' * 30)
+print('Analisador de Triângulos')
+print('=' * 30)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+print('=' * 30)
+if r1 < (r2 + r3) and r2 < (r1 + r3) and r3 < (r1 + r2):
+    print('Formou triângulo')
+else:
+    print('Não formou trinângulo')
+print('=' * 30)
