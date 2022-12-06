a4bc12c168167a181ce09a27b6ba09a460226b03
daslopes <daslopes@outlook.com.br>
2022-12-06 18:11:22 -0300

    Exercício 042 triangulo
    Atualização em andamento.

--- exercicios/ex042.py
+++ exercicios/ex042.py
@@ -2,9 +2,18 @@ ladoA = int(input('Lado A: '))
 ladoB = int(input('Lado B: '))
 ladoC = int(input('Lado C: '))
 
-if ladoA == ladoB and ladoA == ladoC and ladoB == ladoC:
-    print('Equilátero')
-elif ladoA == ladoB or ladoA == ladoC or ladoB == ladoC:
-    print('Isósceles')
+if ladoA < (ladoB + ladoC) and ladoB < (ladoA + ladoC) and ladoC < (ladoA + ladoB):
+    triangulo = 'sim'
+    if ladoA == ladoB and ladoA == ladoC and ladoB == ladoC:
+        tipo = 'Equilátero'
+    elif ladoA == ladoB or ladoA == ladoC or ladoB == ladoC:
+        tipo = 'Isósceles'
+    else:
+        tipo = 'Escaleno'
 else:
-    print('Escaleno')
+    triangulo = 'não'
+
+if triangulo == 'sim':
+    print('Formou trinâgulo: {}, do tipo: {}.'.format(triangulo, tipo))
+else:
+    print('Formou triângulo? {}'.format(triangulo))
