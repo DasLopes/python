1e867f93dbfb63f1f45ca9b4db84e5a549a17423
daslopes <daslopes@outlook.com.br>
2022-12-08 10:39:54 -0300

    jokenpô

---
+++ exercicios/ex045.py
@@ -0,0 +1,22 @@
+from random import choice
+
+lista = ['pedra', 'papel', 'tesoura']
+computador = choice(lista)
+
+jogador = str(input('Jokenpô: '))
+if computador == jogador:
+    print('Jogue novamente')
+elif computador == 'pedra' and jogador == 'tesoura':
+    print('computador ganhou')
+elif computador == 'pedra' and jogador == 'papel':
+    print('jogador ganhou')
+elif computador == 'papel' and jogador == 'pedra':
+    print('computador ganhou')
+elif computador == 'papel' and jogador == 'tesoura':
+    print('jogador ganhou')
+elif computador == 'tesoura' and jogador == 'papel':
+    print('computador ganhou')
+elif computador == 'tesoura' and jogador == 'pedra':
+    print('jogador ganhou')
+else:
+    print('Você deve digitar (pedra, papel ou tesoura)')
