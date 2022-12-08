3818080f709bf24953a2a6d28ce9052e37edf374
daslopes <daslopes@outlook.com.br>
2022-12-08 11:07:33 -0300

    atualização ex036

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -1,10 +1,11 @@
 casa = float(input('Qual é o valor da casa? '))
 salario = float(input('Qual é o seu salário? '))
-tempo = int(input('Em quantos anos deseja pagar? ')) * 12
-prestacao = casa / tempo
-
-print('A prestação do emprestimo será R${:.2f}.'.format(prestacao))
-if prestacao > (salario / 100 * 30):
-    print('Emprestímo negado.')
+tempo = int(input('Em quantos anos deseja pagar? '))
+prestacao = casa / (tempo * 12)
+minimo = salario / 100 * 30
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, tempo))
+print('A prestação será de R${:.2f}'.format(prestacao))
+if prestacao <= minimo:
+    print('Emprestímo APROVADO.')
 else:
-    print('Emprestímo aprovado.')
+    print('Emprestímo NEGADO.')
