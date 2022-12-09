f9b845dfbb408c965ec0f40e53dddb5feaec2302
daslopes <daslopes@outlook.com.br>
2022-12-09 10:42:28 -0300

    atualização ex040

--- exercicios/ex040.py
+++ exercicios/ex040.py
@@ -1,7 +1,7 @@
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
 media = (nota1 + nota2) / 2
-
+print('Com as notas {} e {} sua média será {}'.format(nota1, nota2, media))
 if media >= 7:
     print('APROVADO')
 elif media < 5:
