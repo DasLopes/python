cc05d26b8183bf6d4ea31c8a20227ab81a96f481
daslopes <daslopes@outlook.com.br>
2022-12-09 11:12:06 -0300

    atualização ex042

--- exercicios/ex042.py
+++ exercicios/ex042.py
@@ -2,13 +2,13 @@ ladoA = int(input('Lado A: '))
 ladoB = int(input('Lado B: '))
 ladoC = int(input('Lado C: '))
 
-if ladoA < (ladoB + ladoC) and ladoB < (ladoA + ladoC) and ladoC < (ladoA + ladoB):
+if ladoA < (ladoB + ladoC) or ladoB < (ladoA + ladoC) or ladoC < (ladoA + ladoB):
     print('Os segmentos acima PODEM FORMAR um triângulo do tipo: ', end='')
     if ladoA == ladoB == ladoC:
-        tipo = 'Equilátero'
+        print('Equilátero')
     elif ladoA != ladoB != ladoC != ladoA:
-        tipo = 'Escaleno'
+        print('Escaleno')
     else:
-        tipo = 'Isósceles'
+        print('Isósceles')
 else:
     print('Não formou um triângulo.')
