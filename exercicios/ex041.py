57fd959d64bc076ae8c6fd3c88dfc3b3cc48771d
daslopes <daslopes@outlook.com.br>
2022-12-09 10:49:21 -0300

    atualização ex041

--- exercicios/ex041.py
+++ exercicios/ex041.py
@@ -2,14 +2,14 @@ from datetime import date
 anoAtual = date.today().year
 anoNasc = int(input('Digite seu ano de nascimento: '))
 idade = anoAtual - anoNasc
-
+print('O atleta em {} anos.'.format(idade))
 if idade <= 9:
     print('MIRIM')
 elif idade <= 14:
     print('INFANTIL')
 elif idade <= 19:
     print('JUNIOR')
-elif idade == 20:
+elif idade <= 25:
     print('SÊNIOR')
 else:
     print('MASTER')
