ce5807e646f66326cdd701147399276c7ef47f34
daslopes <daslopes@outlook.com.br>
2022-12-09 11:42:12 -0300

    Atualização ex043

--- exercicios/ex043.py
+++ exercicios/ex043.py
@@ -1,15 +1,15 @@
-peso = float(input('Digite seu peso: '))
-altura = float(input('Digite sua altura: '))
-imc = peso / (altura * altura)
-
+peso = float(input('Qual é seu peso? (Kg) '))
+altura = float(input('Qual é a sua altura? (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
 print('Seu imc: {:.1f}'.format(imc))
 if imc < 18.5:
     print('Abaixo do peso')
-elif imc < 26:
+elif imc < 25:
     print('Peso ideal')
-elif imc < 31:
+elif imc < 30:
     print('Sobrepeso')
-elif imc < 41:
+elif imc < 40:
     print('Obesidade')
 else:
     print('Obesidade mórbida')
