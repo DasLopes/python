d8c3df1faaf0608bcbf813d255dec40a815d060d
daslopes <daslopes@outlook.com.br>
2022-12-14 10:08:27 -0300

    atualização numero impares com contador

--- exercicios/ex048.py
+++ exercicios/ex048.py
@@ -1,7 +1,11 @@
 '''
-Faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+Faça um programa que calcule a soma entre TODOS OS NÚMEROS ÍMPARES que são MÚLTIPLOS DE TRÊS e que se encontram no intervalo de 1 até 500.
 '''
-
-for c in range(1,500):
-    if c % 2 == 1 and c % 3 == 0:
-        print(c)
+soma = 0
+cont = 0
+for c in range(1,501, 2):    
+    if c % 3 == 0:
+        cont += 1
+        soma += c
+print('Entramos {} números.'.format(cont))        
+print('A soma é: {}'.format(soma))
