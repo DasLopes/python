fa5dfdaef937c3b829413f093421304b61727953
daslopes <daslopes@outlook.com.br>
2022-12-14 09:56:28 -0300

    ex047 números pares

--- exercicios/ex047.py
+++ exercicios/ex047.py
@@ -1,8 +1,6 @@
 '''
-Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+Crie um programa que mostre na tela TODOS OS NÚMEROS PARES que estão no intervalo entre 1 e 50.
 '''
-from time import sleep
-for c in range(1,50):
-    if c % 2 == 0:
-        sleep(0.2)
-        print(c)
+
+for c in range(2,51, 2):
+    print(c, end=' ')
