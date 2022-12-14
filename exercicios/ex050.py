e92ae54ffa1785213f189c917684aeae4451e31d
daslopes <daslopes@outlook.com.br>
2022-12-14 10:31:29 -0300

    atualização ex051

--- exercicios/ex050.py
+++ exercicios/ex050.py
@@ -1,8 +1,11 @@
 '''
 Desenvolva um programa que leia SEIS números inteiros e mostre a SOMA apenas daqueles que forem PARES. Se o valor digitado for ímpar, desconsidere-o.'''
-s = 0
-for c in range(0, 6):
-    n = int(input('Digite um número: '))    
+soma = 0
+cont = 0
+for c in range(1, 7):
+    n = int(input('Valor [{}]: '.format(c)))    
     if n % 2 == 0:
-        s = s + n
-print('Soma: {}'.format(s))
+        soma += n
+        cont += 1
+print('Você informou {} números pares.'.format(cont))
+print('A soma dos número pares foi: {}'.format(soma))
