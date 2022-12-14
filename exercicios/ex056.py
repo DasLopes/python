41291640d1bee8fbef23fd4273c50a47e0a84205
daslopes <daslopes@outlook.com.br>
2022-12-14 15:20:25 -0300

    atualização

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -6,18 +6,18 @@ Qual é o nome do homem mais velho.
 Quantas mulheres tem menos de 20 anos idade.
 '''
 totIdade = 0
-pessoas = 2
+pessoas = 4
 maior = 0
 nomeMaior = ' '
 qtdMulheres = 0
-for c in range(0, pessoas):
-    nome = str(input('Digite seu nome: '))
+for c in range(1, pessoas + 1):
+    nome = str(input('Digite seu nome: ')).strip()
     idade = int(input('Digite sua idade: '))
-    sexo = str(input('Qual o sexo: ')).upper()
+    sexo = str(input('Qual o sexo: [M/F')).strip().upper()
 
     totIdade += idade
     if sexo == 'M':
-        if c == 0:
+        if c == 1:
             maior = idade
             nomeMaior = nome
         else:
@@ -28,8 +28,7 @@ for c in range(0, pessoas):
         if idade < 20:
             qtdMulheres += 1
 
-
 media = totIdade / pessoas
 print('O grupo tem uma média de {} anos.'.format(media))
-print('O Sr. {} é o homem mais velho.'.format(nomeMaior))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maior, nomeMaior))
 print('A lista tem {} mulheres abaixo de 20 anos.'.format(qtdMulheres))
---
+++ exercicios/ex056_1.py
@@ -0,0 +1,34 @@
+'''
+Desenvolva um programa que leia o nome, idade, e sexo de 4 pessoas. No final do programa, mostre:
+
+A média de idade do grupo.
+Qual é o nome do homem mais velho.
+Quantas mulheres tem menos de 20 anos idade.
+
+Versão: Professor
+'''
+
+somaIdade = 0
+mediaIdade = 0
+maiorIdadeHomem = 0
+nomeVelho = ''
+totMulher20 = 0
+
+for c in range(1, 5):
+    print('---- {}ª PESSOA -----'.format(c))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaIdade += idade
+    if c == 1 and sexo in 'Mm':
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in 'Mm' and idade > maiorIdadeHomem:
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totMulher20 += 1
+médiaIdade = somaIdade / 4
+print('A média de idade do grupo é de {} anos'.format(médiaIdade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maiorIdadeHomem, nomeVelho))
+print('Ao todo são {} mulheres com menos de 20 anos'.format(totMulher20))
