ac5aef4ad1db82417eecbb43fe14c63d2be625d4
daslopes <daslopes@outlook.com.br>
2022-12-14 11:15:58 -0300

    numero primo

---
+++ exercicios/ex052_1.py
@@ -0,0 +1,12 @@
+'''Faça um programa que leia um número inteiro e diga se ele é ou não um número primo'''
+'''Solução do professor'''
+
+num = int(input('Digite um número: '))
+tot = 0
+for c in range (1, num+1):
+    if num % c == 0:
+        tot += 1
+if tot == 2:
+    print('O número {} é primo.'.format(num))
+else:
+    print('O número {} não é primo.'.format(num))
