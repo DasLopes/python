3b536a2b8588634ca1e0a16c393308becc9321eb
daslopes <daslopes@outlook.com.br>
2022-12-14 10:14:59 -0300

    atualização ex049

--- exercicios/ex049.py
+++ exercicios/ex049.py
@@ -1,6 +1,6 @@
 '''
-Refaça o desafio 009, mostrando oa tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+Refaça o desafio 009, mostrando uma tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
 '''
 n = int(input('Digite um número: '))
-for c in range(1,10+1):
-    print('{} x {} = {}'.format(n, c, (n * c)))
+for c in range(1,11):
+    print('{} x {:2} = {}'.format(n, c, n * c))
