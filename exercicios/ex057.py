437ab1745375c332d479f0ebaec040d59d779d0d
daslopes <daslopes@outlook.com.br>
2022-12-17 11:46:15 -0300

    Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.

---
+++ exercicios/ex057.py
@@ -0,0 +1,9 @@
+'''Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
+
+sx = False
+while sx == False:
+    sexo = str(input('Digite o sexo: [M/F] ')).upper()
+    print(sexo)
+    if sexo == 'F' or sexo == 'M':
+        sx = True    
+print('Obrigado')
