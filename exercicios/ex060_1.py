a33a506ae6a8396bd5950ac6265882c3023412f7
daslopes <daslopes@outlook.com.br>
2022-12-18 16:01:06 -0300

    fatorial com for

---
+++ exercicios/ex060_1.py
@@ -0,0 +1,8 @@
+fatorial = 5#int(input('Digite um número: '))
+print('Fatorial: ', fatorial, ' ', end='')
+cont = fatorial
+for c in range(1, fatorial):
+    fatorial = fatorial * (cont - 1)
+    cont -= 1
+    print(cont, ' ', end='')
+print('= ', fatorial)
