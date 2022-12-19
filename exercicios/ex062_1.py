41c15a500f2a0d0f604d685dcdbee19b97bf04ba
daslopes <daslopes@outlook.com.br>
2022-12-19 15:08:11 -0300

    ex062 versão do professor

---
+++ exercicios/ex062_1.py
@@ -0,0 +1,21 @@
+'''Faça uma progressão aritmética e encerre o programa quando digitar 0
+versão do professor
+'''
+
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais   
+    while cont <= 10:
+        print('{} -> '.format(termo), end='')
+        termo += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
