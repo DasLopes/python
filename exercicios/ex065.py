0892fbed7af2896b58db0a98b7769177b6a858ee
daslopes <daslopes@outlook.com.br>
2022-12-20 09:38:18 -0300

    Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor dos valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,24 +1,22 @@
 '''Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor dos valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.'''
 
-continuar = 'SIM'
-cont = 0
-maior = 0
-menor = 0
-tot = 0
-while continuar == 'SIM':    
+continuar = 'S'
+cont = maior = menor = tot = 0
+while continuar in 'Ss':    
     num = int(input('Digite um número inteiro: '))
     tot += num
-    if cont == 0:
-        maior = num
-        menor = num
+    cont += 1
+    if cont == 1:
+        maior = menor = num
     else:
         if maior < num:
             maior = num
-        elif menor > num:
+        if menor > num:
             menor = num
-    continuar = str(input('Deseja continuar? [SIM/NÃO]')).upper()
-    cont += 1
+    continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    while continuar not in 'SsNn':
+        print('opção inválida')
+        continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()
 media = tot / cont
-print('A média dos números digitados foi: {}'.format(media))
-print('O maior número digitado foi: {}'.format(maior))
-print('O menor número digitado foi: {}'.format(menor))
+print('Você digitou {} números e a média foi {}'.format(cont, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
