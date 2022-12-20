fcba25c44596fbfca9fa3a54e44193d94d785fbe
daslopes <daslopes@outlook.com.br>
2022-12-20 10:54:06 -0300

    Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.

--- exercicios/ex067.py
+++ exercicios/ex067.py
@@ -1,4 +1,4 @@
-'''Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número socilitado for negativo.'''
+'''Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.'''
 
 while True:
     num = int(input('Quer ver a tabuada de qual valor? '))
