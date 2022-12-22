e10eb0fe32e0b9b85e24c64af65b9618d19af624
daslopes <daslopes@outlook.com.br>
2022-12-22 11:13:09 -0300

    Crie uma tupla preenchida com os 20 primeiros colocados da tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:  a) os 5 primeiros. b) Os últimos 4 colocados. c) Times em ordem alfabética. d) Em que posição está o time da Santos.

---
+++ exercicios/ex073.py
@@ -0,0 +1,18 @@
+'''Crie uma tupla preenchida com os 20 primeiros colocados da tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre: 
+a) os 5 primeiros.
+b) Os últimos 4 colocados.
+c) Times em ordem alfabética.
+d) Em que posição está o time da Santos.'''
+
+lista = 'Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Athletico Paranaense', 'Atlético Mineiro', 'Fortaleza', 'São Paulo', 'América', 'Botafogo', 'Santos', 'Goiás', 'Red Bull Bragantino', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético', 'Avaí', 'Juventude'
+
+print('-=' * 20)
+print(f'Lista de times do Brasileirão: {lista}')
+print('-=' * 20)
+print(f'Os 5 primeiros são {lista[:5]}')
+print('-=' * 20)
+print(f'Os 4 últimos são {lista[-4:]}')
+print('-=' * 20)
+print(f'Times em ordem alfabética: {sorted(lista)}')
+print('-=' * 20)
+print(f'O Santos está na {lista.index("Santos")+1}ª posição')
