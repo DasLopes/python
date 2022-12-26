e8a823ba4ab8c412ed9849ecdf5c82cd1342ae59
daslopes <daslopes@outlook.com.br>
2022-12-26 18:14:44 -0300

    versão do professor

---
+++ exercicios/ex083_1.py
@@ -0,0 +1,19 @@
+'''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+versão do professor'''
+
+expr = str(input('Digite a expressão: '))
+pilha = []
+for simp in expr:
+    if simp == '(':
+        pilha.append('(')
+    elif simp == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
