1b3267d99022563b720797715b61e10bfbe270bf
daslopes <daslopes@outlook.com.br>
2022-12-26 11:01:17 -0300

    Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.

---
+++ exercicios/ex082.py
@@ -0,0 +1,21 @@
+'''Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.'''
+lista = []
+pares = []
+impares = []
+while True:
+    lista.append(int(input('Digite um número: ')))
+    while True:
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if continuar in 'SN':
+            break
+    if continuar in 'N':
+        break
+for c in lista:
+    if c % 2 == 0:
+        pares.append(c)
+    else:
+        impares.append(c)
+print('-=' * 30)
+print(f'A lista completa é {lista}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de ímpares é {impares}')
