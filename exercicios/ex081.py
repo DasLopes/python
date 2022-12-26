ab62bd5a5a0f3ba78c433bf404a99d433ec6b9b1
daslopes <daslopes@outlook.com.br>
2022-12-26 10:37:20 -0300

    Atualização ex081

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -3,18 +3,15 @@ a) Quantos números foram digitados.
 b) A lista de valores, ordenada de forma decrescente.
 c) Se o valor 5 foi digitado e está ou não na lista.'''
 lista = []
-cont = 0
 while True:
-    cont += 1
-    n = int(input('Digite um valor: '))
-    lista.append(n)
+    lista.append(int(input('Digite um valor: ')))
     while True:
         r = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
         if r in 'SN':
             break
     if r in 'N':
         break
-print(f'Você digitou {cont} elementos.')
+print(f'Você digitou {len(lista)} elementos.')
 lista.sort(reverse=True)
 print(f'Os valores em ordem decrescente são {lista}')
 if 5 in lista:
