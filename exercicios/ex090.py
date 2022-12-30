4a2f4b6aee809f8e05032e4b1a32ad27df884654
daslopes <daslopes@outlook.com.br>
2022-12-30 16:14:20 -0300

    Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final mostre o conteúdo da estrutura na tela.

---
+++ exercicios/ex090.py
@@ -0,0 +1,15 @@
+'''
+Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final mostre o conteúdo da estrutura na tela. 
+'''
+ficha = {}
+ficha['nome'] = str(input('Nome: '))
+ficha['média'] = float(input(f'Média de {ficha["nome"]}: '))
+if ficha['média'] < 5:
+    ficha['situação'] = 'reprovado'
+elif ficha['média'] < 7:
+    ficha['situação'] = 'recuperação'
+else:
+    ficha['situação'] = 'aprovado'
+print('-=' * 40)
+for k, v in ficha.items():
+    print(f'  - {k} é igual a {v}')
