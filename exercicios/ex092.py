f8adf6b34d6245af5d5fa7daa7d8e36aceaaa6d5
daslopes <daslopes@outlook.com.br>
2022-12-31 10:10:52 -0300

    Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.

--- exercicios/ex092.py
+++ exercicios/ex092.py
@@ -1,17 +1,16 @@
-import datetime
+'''
+Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+'''
 
-currentDateTime = datetime.datetime.now()
-date = currentDateTime.date()
-ano = date.year
+from datetime import datetime
 ficha = {}
 ficha['nome'] = str(input('Nome: '))
-ficha['idade'] = ano - int(input('Ano de Nascimento: '))
+ficha['idade'] = datetime.now().year - int(input('Ano de Nascimento: '))
 ficha['ctps'] = int(input('carteira de Trabalho (0 não tem): '))
 if ficha['ctps'] != 0:
     ficha['contratação'] = int(input('Ano de Contratação: '))
     ficha['Salário'] = float(input('Salário: R$'))
-    ficha['aposentadoria'] = (ficha['contratação'] + 65) - ano
+    ficha['aposentadoria'] = ficha['idade'] + ((ficha['contratação'] + 35) - datetime.now().year)
 print('-=' * 30)
 for k, v in ficha.items():
     print(f'{k} tem o valor {v}')
-
