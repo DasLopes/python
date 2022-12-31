c284d08d089d5cd0d871aeefc204e669068861ad
daslopes <daslopes@outlook.com.br>
2022-12-30 21:45:22 -0300

    ex092 em andamento

---
+++ exercicios/ex092.py
@@ -0,0 +1,18 @@
+import datetime
+
+currentDateTime = datetime.datetime.now()
+date = currentDateTime.date()
+ano = date.year
+ficha = {}
+ficha['nome'] = str(input('Nome: '))
+ficha['idade'] = ano - int(input('Ano de Nascimento: '))
+ficha['ctps'] = int(input('carteira de Trabalho (0 não tem): '))
+if ficha['ctps'] != 0:
+    ficha['contratação'] = int(input('Ano de Contratação: '))
+    ficha['Salário'] = float(input('Salário: R$'))
+    
+
+print('-=' * 30)
+for k, v in ficha.items():
+    print(f'{k} tem o valor {v}')
+
