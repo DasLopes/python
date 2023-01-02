c6bff6042c6c6299da843152557786d480a7f804
daslopes <daslopes@outlook.com.br>
2023-01-02 15:46:00 -0300

    Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,4 +1,6 @@
-
+'''
+Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+'''
 cadastro = dict()
 gols = list()
 jogadores = list()
@@ -8,7 +10,7 @@ while True:
     cadastro['nome'] = str(input('Nome do Jogador: '))
     partidas = int(input(f'Quantas partidas {cadastro["nome"]} jogou? '))
     for c in range(0, partidas):
-        gols.append(int(input(f'\tQuantos gols na partida {c}? ')))    
+        gols.append(int(input(f'\tQuantos gols na partida {c + 1}? ')))    
     cadastro['gols'] = gols[:]
     cadastro['total'] = sum(gols)
     jogadores.append(cadastro.copy())
@@ -18,20 +20,27 @@ while True:
             break
     if resp in 'N':
         break
-print()
 print('-=' * 30)
-print('cod\tnome\t\tgols\t\ttotal')
-print('-' * 50)
-for c in range(0, len(jogadores)):
-    print(f'{c}\t{jogadores[c]}')
-print('-' * 50)
-while True:
-    while True:
-        cod = int(input('Mostrar dados de qual jogador? (999 para parar) '))
-        if cod < len(jogadores) or cod == 999:
-            break
+print('cod ', end='')
+for i in cadastro.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-' * 40)
+for k, v in enumerate(jogadores):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
+while True:    
+    cod = int(input('Mostrar dados de qual jogador? (999 para parar) '))
     if cod == 999:
-        break
-    print(f' -- LEVANTAMENTO DO JOGADOR {jogadores[cod]["nome"]}')
-    for k, v in enumerate(jogadores[cod]['gols']):
-        print(f'No jogo {k + 1} fez {v} gols.')
+        break        
+    if cod >= len(jogadores):
+        print(f'ERRO! Não existe jogador número {cod}')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {jogadores[cod]["nome"]}')
+        for k, v in enumerate(jogadores[cod]['gols']):
+            print(f'No jogo {k + 1} fez {v} gols.')
+    print('-' * 40)
+print('<<< volte sempre >>>')
