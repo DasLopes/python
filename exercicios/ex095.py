9c1a743a343c59b99f9786861aca283f601be730
daslopes <daslopes@outlook.com.br>
2023-01-02 11:59:10 -0300

    atualização ex095

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,9 +1,10 @@
 
 cadastro = dict()
 gols = list()
-jogadores = [{'nome': 'Ronaldinho', 'gols': [2, 3, 2], 'total': 7}, {'nome': 'Ronaldo', 'gols': [3,1], 'total': 4}, {'nome': 'Vampeta', 'gols': [0, 0, 2, 2], 'total': 4}, {'nome': 'Kaká', 'gols': [3], 'total': 3}]
-
-'''while True:
+jogadores = list()
+while True:
+    cadastro.clear()
+    gols.clear()
     cadastro['nome'] = str(input('Nome do Jogador: '))
     partidas = int(input(f'Quantas partidas {cadastro["nome"]} jogou? '))
     for c in range(0, partidas):
@@ -16,7 +17,7 @@ jogadores = [{'nome': 'Ronaldinho', 'gols': [2, 3, 2], 'total': 7}, {'nome': 'Ro
         if resp in 'SN':
             break
     if resp in 'N':
-        break'''
+        break
 print()
 print('-=' * 30)
 print('cod\tnome\t\tgols\t\ttotal')
@@ -24,8 +25,6 @@ print('-' * 50)
 for c in range(0, len(jogadores)):
     print(f'{c}\t{jogadores[c]}')
 print('-' * 50)
-
-
 while True:
     while True:
         cod = int(input('Mostrar dados de qual jogador? (999 para parar) '))
@@ -34,12 +33,5 @@ while True:
     if cod == 999:
         break
     print(f' -- LEVANTAMENTO DO JOGADOR {jogadores[cod]["nome"]}')
-    for c in jogadores[cod]:
-        if c in gols:
-            for i in c:
-                print(f'{i} - {c[i]}')
-
-'''print(f'O jogador {cadastro["nome"]} jogou {partidas} partidas.')
-for i, v in enumerate(gols):
-    print(f'\t=> Na partica {i}, fez {v} gols.')
-print(f'Foi um total de {cadastro["total"]} gols.')'''
+    for k, v in enumerate(jogadores[cod]['gols']):
+        print(f'No jogo {k + 1} fez {v} gols.')
