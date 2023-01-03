d7453f9a74218de7d49ed0421286f2cebfdcafae
daslopes <daslopes@outlook.com.br>
2023-01-03 15:57:48 -0300

    atualização aula

---
+++ aulas/aula20.py
@@ -0,0 +1,34 @@
+def lin():
+    print('-' * 222)
+
+def mensagem(msg):
+    lin()
+    print(msg)
+    lin()
+
+
+
+# Programa Principal
+mensagem('   Curso em Vídeo')
+mensagem('   APRENDA PYTHON')
+mensagem('   PYTHON É MUITO BOM!')
+mensagem('Oi')
+
+############################################################### PRÁTICA 1 #####################################################################
+def pratica(txt):
+    print(f'{txt}' * 222)
+
+pratica('§')
+
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+
+
+
+soma((37+34), 30)
+lin()
+soma(8,9)
+lin()
+soma(2,1)
