694e57da1e98817b4b054b5e4bdd57cc4f6e44b6
daslopes <daslopes@outlook.com.br>
2023-01-04 10:20:41 -0300

    organização de código

--- exercicios/ex099.py
+++ exercicios/ex099.py
@@ -5,8 +5,8 @@ def maior(* valor):
         print(f'{num}', end=' ')
     print(f'Foram informados {len(valor)} valores ao todo.')        
     print(f'O maior valor informado foi {max(valor)}')
-    
 
+    
 maior(2,9,4,5,7,1)
 maior(4,7,0)
 maior(1,2)
