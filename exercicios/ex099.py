7fd752eda1e3ec42072787c0cd02677cee429001
daslopes <daslopes@outlook.com.br>
2023-01-04 10:12:11 -0300

    minha solução ex099

---
+++ exercicios/ex099.py
@@ -0,0 +1,13 @@
+def maior(* valor):
+    print('-=' * 30)
+    print('Analisando os valores passados...')    
+    for num in valor:
+        print(f'{num}', end=' ')
+    print(f'Foram informados {len(valor)} valores ao todo.')        
+    print(f'O maior valor informado foi {max(valor)}')
+    
+
+maior(2,9,4,5,7,1)
+maior(4,7,0)
+maior(1,2)
+maior(0)
