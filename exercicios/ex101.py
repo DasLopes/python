b5f850006acae31044dab38cfc54eef09e86d6d5
daslopes <daslopes@outlook.com.br>
2023-01-06 16:24:44 -0300

    conclusão exercício

---
+++ exercicios/ex101.py
@@ -0,0 +1,16 @@
+def voto(ano = 0):
+    from datetime import datetime
+    global idade
+    anoAtual = datetime.now().year    
+    idade = anoAtual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA'
+    elif idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL'    
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+print('-' * 40)
+nasc = int(input('Em que ano você nasceu? '))
+print(voto(nasc))
+
