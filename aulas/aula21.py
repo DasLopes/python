8e7de89994df050faf2287cf013c9fc1be0e3192
daslopes <daslopes@outlook.com.br>
2023-01-06 15:42:01 -0300

    atualização aula 21

--- aulas/aula21.py
+++ aulas/aula21.py
@@ -79,7 +79,7 @@ teste(a)
 print(f'A fora vale {a}')
 '''
 #################################### FUNÇÕES COM RETORNO ########################################
-
+'''
 def somar(a=0, b=0, c=0):
     s = a + b + c
     return s
@@ -89,3 +89,33 @@ r2 = somar(2,2)
 r3 = somar(6)
 
 print(f'Os resutlados foram {r1}, {r2} e {r3}.')
+'''
+
+#################################### PRÁTICA FUNÇÕES COM RETORNO ########################################
+'''
+def fatorial (num = 0):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2} e {f3}')
+'''
+
+#################################### PRÁTICA FUNÇÕES COM RETORNO LÓGICO ########################################
+
+def par(num = 0):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+num = int(input('Digite um número: '))
+if par(num):
+    print('É par!')
+else:
+    print('Não é par!')
