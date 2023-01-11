cd2fbbc34be36015c52e26498bd6aa4894486ebf
daslopes <daslopes@outlook.com.br>
2023-01-11 12:39:06 -0300

    ex108 concluído

--- ex108/moeda.py
+++ ex108/moeda.py
@@ -1,11 +1,14 @@
-def metade(preço):
+def metade(preço = 0):
     return preço / 2
 
-def dobro(preço):
+def dobro(preço = 0):
     return preço * 2
 
-def aumentar(preço, taxa=0):
+def aumentar(preço = 0, taxa = 0):
     return preço + (preço / 100 * taxa)
 
-def diminuir(preço, taxa=0):
+def diminuir(preço = 0, taxa = 0):
     return preço - (preço / 100 * taxa)
+
+def moeda(preço = 0, moeda = 'R$'):
+    return f'{moeda}{preço:.2f}'.replace('.', ',')
--- ex108/teste.py
+++ ex108/teste.py
@@ -1,7 +1,7 @@
 import moeda
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de R${p} é R${moeda.metade}')
-print(f'O dobro de R${p} é R${moeda.dobro}')
-print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10)}')
-print(f'Diminuindo 10%, temos R${moeda.diminuir(p, 10)}')
+print(f'A metade de R${moeda.moeda(p)} é R${moeda.moeda(moeda.metade(p))}')
+print(f'O dobro de R${moeda.moeda(p)} é R${moeda.moeda(moeda.dobro(p))}')
+print(f'Aumentando 10%, temos R${moeda.moeda(moeda.aumentar(p, 10))}')
+print(f'Diminuindo 10%, temos R${moeda.moeda(moeda.diminuir(p, 10))}')
