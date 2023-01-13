953f394740d455e8667e34c44c4acf69bbe80efa
daslopes <daslopes@outlook.com.br>
2023-01-13 14:52:09 -0300

    ex114 atualização

--- exercicios/ex114.py
+++ exercicios/ex114.py
@@ -7,3 +7,5 @@ except urllib.error.URLError:
     print('O site pudim não está acessivel no momento.')
 else:
     print('Consegui acessar o site Pudim com sucesso.')
+    print(site.read()) #faz a leitura do site e mostra seu conteúdo
+    
