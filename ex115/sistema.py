97089f09f4b96599aaa4d2d9ee0cf5f8391ec4cf
daslopes <daslopes@outlook.com.br>
2023-01-13 17:35:46 -0300

    ex115 função de cadastro e organização de dados na visualização

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -7,11 +7,18 @@ if not arquivoExiste(arq):
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
     if resposta == 1:
+        # Opção de listar o conteúdo de um arquivo.
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('opção 2')
+        # Opção de cadastrar uma nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabeçalho('Saindo do Sistema... Até logo!')
         break
     else:
+        # Digitou uma opção errada no sistema.
         print('\033[31mERRO! Digite uma opção válida.\033[m')
