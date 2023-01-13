31fc41c11b30a9f3c442e27b189ac259e414a74c
daslopes <daslopes@outlook.com.br>
2023-01-13 14:48:23 -0300

    ex114 atualizado

---
+++ exercicios/ex114.py
@@ -0,0 +1,9 @@
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site pudim não está acessivel no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso.')
