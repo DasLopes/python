ed39e8b72e1bcd272bb961552b110dad87c6269c
daslopes <daslopes@outlook.com.br>
2023-01-14 10:25:14 -0300

    Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.

--- exercicios/ex054.py
+++ exercicios/ex054.py
@@ -1,6 +1,7 @@
 '''
 Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
 '''
+
 from datetime import date
 anoAtual = date.today().year
 menor = 0
