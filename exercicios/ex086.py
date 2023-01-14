6b5ccb91ba311ddf56533d0eea8d9cf96fcc2b7d
daslopes <daslopes@outlook.com.br>
2023-01-14 11:02:46 -0300

    Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.

--- exercicios/ex086.py
+++ exercicios/ex086.py
@@ -1,5 +1,6 @@
-'''Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
-No final, mostre a matriz na tela, com a formatação correta.'''
+'''
+Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+'''
 
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
 for l in range(0,3):
