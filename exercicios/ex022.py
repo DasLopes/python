18e8671256224653df6a27de5f19c80194d7811c
daslopes <daslopes@outlook.com.br>
2023-01-14 09:47:09 -0300

    Crie um programa que leia o nome completo de uma pessoa e mostre:  - O nome com todas as letras maiúsculas e minúsculas. - Quantas letras ao todo (sem considerar espaços). - Quantas letras tem o primeiro nome.

--- exercicios/ex022.py
+++ exercicios/ex022.py
@@ -1,3 +1,10 @@
+'''
+Crie um programa que leia o nome completo de uma pessoa e mostre: 
+- O nome com todas as letras maiúsculas e minúsculas.
+- Quantas letras ao todo (sem considerar espaços).
+- Quantas letras tem o primeiro nome.
+'''
+
 nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome...')
 print('Seu nome em maiúsculas é {}'.format(nome.upper()))
