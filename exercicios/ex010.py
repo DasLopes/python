2a0c44a064819776cbfe0b5b0ff6030a2ca70d3e
daslopes <daslopes@outlook.com.br>
2023-01-14 09:36:00 -0300

    Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+'''
+
 real = float(input('Quanto dinheiro você tem na carteira? R$ '))
 cotacao = 5.40
 dolar = real / cotacao
