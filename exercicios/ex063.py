684eab3ac5e388eb6895cc08d40cd25f84968bac
daslopes <daslopes@outlook.com.br>
2023-01-14 10:38:52 -0300

    Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci.

--- exercicios/ex063.py
+++ exercicios/ex063.py
@@ -1,7 +1,6 @@
-'''Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma Sequência Fibonacci.
-
-Ex:
-0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8'''
+'''
+Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci.
+'''
 
 termo = int(input('Digite um número inteiro: '))
 fib1 = 0
