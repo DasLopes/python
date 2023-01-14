a804bf6b9c530053046793c2b692eea38771c074
daslopes <daslopes@outlook.com.br>
2023-01-14 11:14:22 -0300

    Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.

--- exercicios/ex099.py
+++ exercicios/ex099.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+'''
+
 def maior(* valor):
     print('-=' * 30)
     print('Analisando os valores passados...')    
