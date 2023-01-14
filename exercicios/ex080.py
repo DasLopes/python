fd44bdfc858a5bdf693e26cec280903a292b7edf
daslopes <daslopes@outlook.com.br>
2023-01-14 10:58:01 -0300

    Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.

--- exercicios/ex080.py
+++ exercicios/ex080.py
@@ -1,4 +1,6 @@
-'''Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista. já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.'''
+'''
+Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+'''
 
 lista = []
 for c in range(0, 5):
