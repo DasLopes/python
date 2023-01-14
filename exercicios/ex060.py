2852baec90ab621ff05ac0dcbd28f1ca8db732c3
daslopes <daslopes@outlook.com.br>
2023-01-14 10:36:13 -0300

    Faça um programa que leia um número qualquer e mostre o seu fatorial.

--- exercicios/ex060.py
+++ exercicios/ex060.py
@@ -1,7 +1,6 @@
-'''Faça um programa que leia um número qualquer e mostre o seu fatorial
-
-Ex:
-5!=5x4x3x2x1=120'''
+'''
+Faça um programa que leia um número qualquer e mostre o seu fatorial.
+'''
 
 fatorial = int(input('Digite um número: '))
 print('Fatorial {}!: {} x '.format(fatorial, fatorial), end='')
