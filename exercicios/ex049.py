ed26717e47393e5485ea1414c2991912fed31ff8
daslopes <daslopes@outlook.com.br>
2023-01-14 10:20:02 -0300

    Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.

--- exercicios/ex049.py
+++ exercicios/ex049.py
@@ -1,5 +1,5 @@
 '''
-Refaça o desafio 009, mostrando uma tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
 '''
 n = int(input('Digite um número: '))
 for c in range(1,11):
