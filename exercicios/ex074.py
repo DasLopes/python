e6fb3eeefcc146658f49e1bbdf86f90a82b9b076
daslopes <daslopes@outlook.com.br>
2023-01-14 10:51:12 -0300

    Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -1,4 +1,6 @@
-'''Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla'''
+'''
+Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+'''
 
 from random import randint
 tupla = (randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9))
