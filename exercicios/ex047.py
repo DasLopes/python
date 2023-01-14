27c575ac8bac73e83a13a9cde02f84951a324693
daslopes <daslopes@outlook.com.br>
2023-01-14 10:18:41 -0300

    Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.

--- exercicios/ex047.py
+++ exercicios/ex047.py
@@ -1,5 +1,5 @@
 '''
-Crie um programa que mostre na tela TODOS OS NÚMEROS PARES que estão no intervalo entre 1 e 50.
+Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 '''
 
 for c in range(2,51, 2):
