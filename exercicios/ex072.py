bb8c385ac5b8999d4483ff7d0326dd0d6c4f0461
daslopes <daslopes@outlook.com.br>
2023-01-14 10:49:41 -0300

    Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.

--- exercicios/ex072.py
+++ exercicios/ex072.py
@@ -1,6 +1,6 @@
-'''Crie um programa que tenha um tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
-
-Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.'''
+'''
+Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+'''
 
 lista = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
 
