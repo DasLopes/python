4c903abfac65552e107e69d9216eb302e2b13f62
daslopes <daslopes@outlook.com.br>
2023-01-14 09:47:41 -0300

    Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+'''
+
 num = int(input('Digite um número: '))
 u = num // 1 % 10
 d = num // 10 % 10
