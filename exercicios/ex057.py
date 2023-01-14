9cbb7e90907a42bea70aca41074387f700ac92b4
daslopes <daslopes@outlook.com.br>
2023-01-14 10:27:28 -0300

    Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.

--- exercicios/ex057.py
+++ exercicios/ex057.py
@@ -1,4 +1,6 @@
-'''Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
+'''
+Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+'''
 
 sx = False
 while sx == False:
