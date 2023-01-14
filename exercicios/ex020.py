ae0b95188e4e2102efe04cd45cfdf44fd41326f4
daslopes <daslopes@outlook.com.br>
2023-01-14 09:45:40 -0300

    O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,8 +1,6 @@
-# o mesmo professor do desafio anterior
-# quer sortear a ordem de apresentação de
-# trabalhos dos alunos.
-# faça um programa que leia o nome dos alunos
-# e mostre a ordem sorteada.
+'''
+O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+'''
 
 from random import shuffle
 
