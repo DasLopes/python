fb7acec2312c1bb41e1f9f51255ab41c41b71114
daslopes <daslopes@outlook.com.br>
2023-01-14 10:28:09 -0300

    Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.

--- exercicios/ex058.py
+++ exercicios/ex058.py
@@ -1,4 +1,6 @@
-'''Faça o computador pensar em um número entre 0 e 10. O jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.'''
+'''
+Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+'''
 
 import random, time
 
