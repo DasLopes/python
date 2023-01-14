5e860e32be8841bb60aa847bcad990103f597247
daslopes <daslopes@outlook.com.br>
2023-01-14 10:13:42 -0300

    Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.

--- exercicios/ex046.py
+++ exercicios/ex046.py
@@ -1,5 +1,5 @@
 '''
-Faça um programa que mostre na tela uma contahem regressiva para o estouro de fogos de artificio, indo de 10 até 0, com com uma pausa de 1 segundo entre eles.
+Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 '''
 
 from time import sleep
