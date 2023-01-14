a643322a2db61c988381f79ded23e1d3b09fff32
daslopes <daslopes@outlook.com.br>
2023-01-14 11:16:18 -0300

    Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.

--- exercicios/ex102.py
+++ exercicios/ex102.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+'''
+
 def fatorial(n, show=False):
     """
     -> Calcula o Fatorial de um número.
