95df09c0c639e80e9067dcab0ed1d64cbb29fb32
daslopes <daslopes@outlook.com.br>
2023-01-14 09:37:13 -0300

    Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -1,3 +1,7 @@
+'''
+Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+'''
+
 preco = float(input('Qual é o preço do produto? R$'))
 desc = float(input('Qual é o percentual de desconto? '))
 npreco = preco - (preco / 100 * desc)
