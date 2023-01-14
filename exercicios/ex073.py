cd339d181216d7f1473b2d22b078de69876eb375
daslopes <daslopes@outlook.com.br>
2023-01-14 10:50:39 -0300

    Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre: a) Os 5 primeiros times. b) Os últimos 4 colocados. c) Times em ordem alfabética.  d) Em que posição está o time da Chapecoense.

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -1,8 +1,10 @@
-'''Crie uma tupla preenchida com os 20 primeiros colocados da tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre: 
-a) os 5 primeiros.
+'''
+Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+a) Os 5 primeiros times.
 b) Os últimos 4 colocados.
-c) Times em ordem alfabética.
-d) Em que posição está o time da Santos.'''
+c) Times em ordem alfabética. 
+d) Em que posição está o time da Santos.
+'''
 
 lista = 'Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Athletico Paranaense', 'Atlético Mineiro', 'Fortaleza', 'São Paulo', 'América', 'Botafogo', 'Santos', 'Goiás', 'Red Bull Bragantino', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético', 'Avaí', 'Juventude'
 
