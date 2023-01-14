6be664e3f06dd0b1a741bd1f22ab94254a90508f
daslopes <daslopes@outlook.com.br>
2023-01-14 10:41:00 -0300

    Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,4 +1,6 @@
-'''Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor dos valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.'''
+'''
+Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+'''
 
 continuar = 'S'
 cont = maior = menor = tot = 0
