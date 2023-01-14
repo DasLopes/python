01c463eb53c1e94198fce20f02a64d26bd67214b
daslopes <daslopes@outlook.com.br>
2023-01-14 09:49:30 -0300

    Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.

--- exercicios/ex026.py
+++ exercicios/ex026.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+'''
+
 frase = str(input('Digite uma frase: ')).strip().upper()
 print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
 print('A primeira letra A apareceu na posição {}'.format(frase.find('A') + 1))
