d46696e0c0af77e3f084ff1739e16106bdd8b227
daslopes <daslopes@outlook.com.br>
2023-01-14 10:57:06 -0300

    Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -1,5 +1,6 @@
-'''Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro ele não será adicionado.
-No final, serão exibidos todos os valores únicos digitados, em ordem crescente.'''
+'''
+Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+'''
 
 números = []
 while True:
