b2015ec13316cb526d9006285fa50c000f0521e6
daslopes <daslopes@outlook.com.br>
2023-01-14 09:33:32 -0300

    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -1,3 +1,7 @@
+'''
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+'''
+
 n1 = float(input('Primeira nota do aluno: '))
 n2 = float(input('Segunda nota do aluno: '))
 m = (n1 + n2) / 2
