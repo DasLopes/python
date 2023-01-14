35d9ab467bab0374aa4911de6d756e6af1fec1d7
daslopes <daslopes@outlook.com.br>
2023-01-14 11:10:55 -0300

    Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:  A) Quantas pessoas foram cadastradas B) A média de idade C) Uma lista com as mulheres D) Uma lista de pessoas com idade acima da média

--- exercicios/ex094.py
+++ exercicios/ex094.py
@@ -1,9 +1,9 @@
 '''
-Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
-a) Quantos pessoas cadastrados.
-b) A média de idade.
-c) Uma lista com mulheres.
-d) Uma lista com idade acima da média.
+Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: 
+A) Quantas pessoas foram cadastradas
+B) A média de idade
+C) Uma lista com as mulheres
+D) Uma lista de pessoas com idade acima da média
 '''
 
 dict = {}
