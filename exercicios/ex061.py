6fca40d152b64948642e33b8adba336a30b004a8
daslopes <daslopes@outlook.com.br>
2023-01-14 10:37:28 -0300

    Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.

--- exercicios/ex061.py
+++ exercicios/ex061.py
@@ -1,4 +1,6 @@
-'''Mostre os 10 primeiros termos da progressão aritimetica usando a estrutura while'''
+'''
+Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
+'''
 
 primeiro = 1#int(input('Primeiro termo: '))
 razão = 1#int(input('Razão: '))
