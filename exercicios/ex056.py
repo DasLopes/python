c788b0b6ce896fb699e3ad73529c91711fc48984
daslopes <daslopes@outlook.com.br>
2023-01-14 10:26:27 -0300

    Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -1,9 +1,5 @@
 '''
-Desenvolva um programa que leia o nome, idade, e sexo de 4 pessoas. No final do programa, mostre:
-
-A média de idade do grupo.
-Qual é o nome do homem mais velho.
-Quantas mulheres tem menos de 20 anos idade.
+Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 '''
 totIdade = 0
 pessoas = 4
