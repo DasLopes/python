9787bc0a6fc38cc630d82eac07c142944088ab42
daslopes <daslopes@outlook.com.br>
2023-01-14 10:51:58 -0300

    Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:  A) Quantas vezes apareceu o valor 9. B) Em que posição foi digitado o primeiro valor 3. C) Quais foram os números pares.

--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -1,8 +1,10 @@
-'''Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+'''
+Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
 
-a) Quantas vezes apareceu o valor 9.
-b) Em que posição foi digitado o primeiro valor 3.
-c) Quais foram os números pares.'''
+A) Quantas vezes apareceu o valor 9.
+B) Em que posição foi digitado o primeiro valor 3.
+C) Quais foram os números pares.
+'''
 
 lista = (   int(input('Digite um número: ')), 
             int(input('Digite outro número: ')),
