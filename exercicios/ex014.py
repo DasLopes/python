0ac487f0fbd7dc9347cfe131eaab771a4262a7b6
daslopes <daslopes@outlook.com.br>
2023-01-14 09:40:44 -0300

    Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.

--- exercicios/ex014.py
+++ exercicios/ex014.py
@@ -1,3 +1,7 @@
+'''
+Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+'''
+
 temp = float(input('Informe a temperatura em ºC: '))
 novo = temp * 1.8 + 32
 print('A temperatura de {}ºC corresponde a {}ºF!'.format(temp, novo))
