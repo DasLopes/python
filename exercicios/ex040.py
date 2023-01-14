171cd26289313fe47db5b42b1318990b1bc34d70
daslopes <daslopes@outlook.com.br>
2023-01-14 10:09:01 -0300

    Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida: - Média abaixo de 5.0: REPROVADO - Média entre 5.0 e 6.9: RECUPERAÇÃO - Média 7.0 ou superior: APROVADO

--- exercicios/ex040.py
+++ exercicios/ex040.py
@@ -1,3 +1,10 @@
+'''
+Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+- Média abaixo de 5.0: REPROVADO
+- Média entre 5.0 e 6.9: RECUPERAÇÃO
+- Média 7.0 ou superior: APROVADO
+'''
+
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
 media = (nota1 + nota2) / 2
