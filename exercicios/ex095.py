e6b5dae2ed78b0fee12afdc98e9858f5be9f8cf2
daslopes <daslopes@outlook.com.br>
2023-01-14 11:11:30 -0300

    Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,6 +1,7 @@
 '''
-Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 '''
+
 cadastro = dict()
 gols = list()
 jogadores = list()
