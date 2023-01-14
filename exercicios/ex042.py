6da8abf01c8cd656f6b757c725f70532fdb83b17
daslopes <daslopes@outlook.com.br>
2023-01-14 10:10:36 -0300

    Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: - EQUILÁTERO: todos os lados iguais - ISÓSCELES: dois lados iguais, um diferente - ESCALENO: todos os lados diferentes

--- exercicios/ex042.py
+++ exercicios/ex042.py
@@ -1,3 +1,10 @@
+'''
+Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+- EQUILÁTERO: todos os lados iguais
+- ISÓSCELES: dois lados iguais, um diferente
+- ESCALENO: todos os lados diferentes
+'''
+
 ladoA = int(input('Lado A: '))
 ladoB = int(input('Lado B: '))
 ladoC = int(input('Lado C: '))
