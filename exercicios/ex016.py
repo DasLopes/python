c9f6f7023f8cb1bb757d945e18f93db40a4685b8
daslopes <daslopes@outlook.com.br>
2023-01-14 09:42:40 -0300

    Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.

--- exercicios/ex016.py
+++ exercicios/ex016.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+'''
+
 '''from math import trunc
 num = float(input('Digite um número: '))
 print('O número {} tem a parte inteira {}'.format(num, trunc(num)))
