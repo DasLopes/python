e63345d578bf6167bfc9016f018425077db39d25
daslopes <daslopes@outlook.com.br>
2023-01-14 09:32:56 -0300

    Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

--- exercicios/ex006.py
+++ exercicios/ex006.py
@@ -1,3 +1,7 @@
+'''
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+'''
+
 n = int(input('Digite um número: '))
 
 print('O dobro de {} vale {}.'.format(n, (n*2)))
