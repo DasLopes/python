6610230e4332ab0be8a15e3582005a637699ed5d
daslopes <daslopes@outlook.com.br>
2023-01-14 11:20:51 -0300

    Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.

--- ex108/teste.py
+++ ex108/teste.py
@@ -1,3 +1,7 @@
+'''
+Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
+'''
+
 import moeda
 
 p = float(input('Digite o preço: R$'))
