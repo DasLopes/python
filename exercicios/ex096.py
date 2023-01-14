0c5afaf5bf3864445fa6f29fb7ce477a4fa5f2ff
daslopes <daslopes@outlook.com.br>
2023-01-14 11:12:09 -0300

    Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.

--- exercicios/ex096.py
+++ exercicios/ex096.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+'''
+
 def area(l, c):
     s = l * c
     print(f'A área de um terreno {l:.1f} x {c:.1f} é de {s:.1f}m².')
