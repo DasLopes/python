605611bb83e0921951a9019c0c640ab9a60ede93
daslopes <daslopes@outlook.com.br>
2023-01-14 10:22:23 -0300

    Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.

--- exercicios/ex052.py
+++ exercicios/ex052.py
@@ -1,6 +1,5 @@
 '''
-Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
-'''
+Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.'''
 
 n = int(input('Digite um número: '))
 for c in range(1, n):
