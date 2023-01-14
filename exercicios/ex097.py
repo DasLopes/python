360bdd14d11321f17e5f48882278e02015353bc5
daslopes <daslopes@outlook.com.br>
2023-01-14 11:13:08 -0300

    Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.

--- exercicios/ex097.py
+++ exercicios/ex097.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+'''
+
 def escreva(msg):
     print('~' * len(msg))
     print(msg)
