9543d6285ba2fec2aab93b1e834e2a971d2d3730
daslopes <daslopes@outlook.com.br>
2023-01-14 09:45:01 -0300

    Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.

--- exercicios/ex019.py
+++ exercicios/ex019.py
@@ -1,8 +1,4 @@
-'''um professor quer sortear um dos seus
-quatro alunos para apagar o quadro.
-Faça um programa que ajude ele,
-lendo o nome deles e escrevendo
-o nome do escolhido.'''
+'''Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.'''
 from random import choice
 a1 = str(input('Primeiro aluno: '))
 a2 = str(input('Segundo aluno: '))
