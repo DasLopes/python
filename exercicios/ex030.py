fca70969fa46dac5b4fbd030ed6461268866f245
daslopes <daslopes@outlook.com.br>
2023-01-14 09:53:56 -0300

    Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.

--- exercicios/ex030.py
+++ exercicios/ex030.py
@@ -1,6 +1,5 @@
 '''
-Crie um programa que leia um número inteiro
-e mostre na tela se ele é PAR ou ÍMPAR.
+Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 '''
 
 num = int(input('Digite um númeiro: '))
