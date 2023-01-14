b7a6c2bc4ecced9842062c9e1ac2fbd2e9748b79
daslopes <daslopes@outlook.com.br>
2023-01-14 11:19:54 -0300

    Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.

--- ex107/teste.py
+++ ex107/teste.py
@@ -1,3 +1,7 @@
+'''
+Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
+'''
+
 #import ex107.moeda as moeda #codigo editado pelo vsconde
 import moeda
 
