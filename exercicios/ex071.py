4e36289c7b130f8266a40feea71b11e900cce586
daslopes <daslopes@outlook.com.br>
2023-01-14 10:48:55 -0300

    Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.

--- exercicios/ex071.py
+++ exercicios/ex071.py
@@ -1,6 +1,7 @@
-'''Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
-
-Observação: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
+'''
+Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
+OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+'''
 
 print('=' * 40)
 print(' ' * 15, 'BANCO CEV')
