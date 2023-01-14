93ea7b995ee26738fd8b0d14c51c8933714a0c04
daslopes <daslopes@outlook.com.br>
2023-01-14 11:17:26 -0300

    Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante 'a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt('Digite um n: ')

--- exercicios/ex104.py
+++ exercicios/ex104.py
@@ -1,3 +1,8 @@
+'''
+Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante 'a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.
+Ex: n = leiaInt('Digite um n: ')
+'''
+
 def leiaInt(msg):   
     while True:
         n = str(input(msg))
