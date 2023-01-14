4296e519552ac5b129dbca6b6c0c8fb0d27374bf
daslopes <daslopes@outlook.com.br>
2023-01-14 10:21:40 -0300

    Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.

--- exercicios/ex051.py
+++ exercicios/ex051.py
@@ -1,6 +1,5 @@
 '''
-Desenvolva um programa que leia o primeiro termo e a razão de uma PA (progressão aritmética). No final, mostre os 10 primeiros termos dessa progressão.
-ex: (1, 4, 7, 10, 13, ...) a1 é igual a 1 e a razão r é igual a 3.
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
 '''
 primeiro = int(input('Primeiro termo: '))
 razão = int(input('Razão: '))
