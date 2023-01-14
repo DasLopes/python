da90ca404ff098c05dd59b962bdb98e7be563461
daslopes <daslopes@outlook.com.br>
2023-01-14 09:59:16 -0300

    Faça um programa que leia três números e mostre qual é o maior e qual é o menor.

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+'''
+
 a = int(input('Primeiro valor: '))
 b = int(input('Segundo valor: '))
 c = int(input('Terceiro valor: '))
