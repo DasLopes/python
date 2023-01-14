4c271df74bc7c58a23ae0208fd0e878197025df8
daslopes <daslopes@outlook.com.br>
2023-01-14 11:00:33 -0300

    Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.

--- exercicios/ex083.py
+++ exercicios/ex083.py
@@ -1,4 +1,6 @@
-'''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
+'''
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+'''
 
 expr = '((a+b)*c)(x+y(3+2/3)*z)'#'((((a+b)*c)+2)/4))' #str(input('Digite a expressão: '))
 a = b = 0
