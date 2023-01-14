83e4eed978ab94f5af90ed70d8cf64b472d78af1
daslopes <daslopes@outlook.com.br>
2023-01-14 11:16:53 -0300

    Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.

--- exercicios/ex103.py
+++ exercicios/ex103.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+'''
+
 def ficha(jog='<desconhecido>', gol = 0):
     print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
 
