d993f81726db8d06b96f9145fefeaf2222303c62
daslopes <daslopes@outlook.com.br>
2023-01-14 09:31:30 -0300

    Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.

--- exercicios/ex004.py
+++ exercicios/ex004.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+'''
+
 a = input('Digite algo: ')
 print('O tipo primitivo desse valor é ', type(a))
 print('Só tem espaços? ', a.isspace())
