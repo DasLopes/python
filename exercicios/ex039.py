06d78345c924a10cc4eb48a9799ae9906f966da3
daslopes <daslopes@outlook.com.br>
2023-01-14 10:08:25 -0300

    Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.

--- exercicios/ex039.py
+++ exercicios/ex039.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+'''
+
 from datetime import date
 anoAtual = date.today().year
 anoNasc = int(input('Em que ano nasceu? '))
