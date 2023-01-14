6421ee3b34984da014ba560334bdf13c5430ce73
daslopes <daslopes@outlook.com.br>
2023-01-14 09:48:22 -0300

    Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+'''
+
 cid = str(input('Em que cidade você nasceu? ')).strip().upper()
 print('SANTO' in cid) #primeira solução
 print(cid[:5] == 'SANTO') #segunda solução
