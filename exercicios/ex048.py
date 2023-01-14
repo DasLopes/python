c125dcd2fbdb75952618396bb436be560ab6054a
daslopes <daslopes@outlook.com.br>
2023-01-14 10:19:27 -0300

    Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.

--- exercicios/ex048.py
+++ exercicios/ex048.py
@@ -1,5 +1,5 @@
 '''
-Faça um programa que calcule a soma entre TODOS OS NÚMEROS ÍMPARES que são MÚLTIPLOS DE TRÊS e que se encontram no intervalo de 1 até 500.
+Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 '''
 soma = 0
 cont = 0
