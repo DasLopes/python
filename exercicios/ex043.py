c1ee5d0009b086991f3a1b1376e76eb390ca5aa0
daslopes <daslopes@outlook.com.br>
2023-01-14 10:11:10 -0300

    Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo: - IMC abaixo de 18,5: Abaixo do Peso - Entre 18,5 e 25: Peso Ideal - 25 até 30: Sobrepeso - 30 até 40: Obesidade - Acima de 40: Obesidade Mórbida

--- exercicios/ex043.py
+++ exercicios/ex043.py
@@ -1,3 +1,12 @@
+'''
+Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
+- IMC abaixo de 18,5: Abaixo do Peso
+- Entre 18,5 e 25: Peso Ideal
+- 25 até 30: Sobrepeso
+- 30 até 40: Obesidade
+- Acima de 40: Obesidade Mórbida
+'''
+
 peso = float(input('Qual é seu peso? (Kg) '))
 altura = float(input('Qual é a sua altura? (m) '))
 imc = peso / (altura ** 2)
