bd1fbcb2d709809dd251af085652b7b9501e7b35
daslopes <daslopes@outlook.com.br>
2023-01-14 10:58:43 -0300

    Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre: A) Quantos números foram digitados. B) A lista de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado e está ou não na lista.

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -1,7 +1,10 @@
-'''Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
-a) Quantos números foram digitados.
-b) A lista de valores, ordenada de forma decrescente.
-c) Se o valor 5 foi digitado e está ou não na lista.'''
+'''
+Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+A) Quantos números foram digitados.
+B) A lista de valores, ordenada de forma decrescente.
+C) Se o valor 5 foi digitado e está ou não na lista.
+'''
+
 lista = []
 while True:
     lista.append(int(input('Digite um valor: ')))
