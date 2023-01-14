2da3989db34b5f4230096b879990dd5e27a8fc2f
daslopes <daslopes@outlook.com.br>
2023-01-14 10:47:31 -0300

    Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre: A) quantas pessoas tem mais de 18 anos. B) quantos homens foram cadastrados. C) quantas mulheres tem menos de 20 anos.

--- exercicios/ex069.py
+++ exercicios/ex069.py
@@ -1,8 +1,10 @@
-'''Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
+'''
+Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
+A) quantas pessoas tem mais de 18 anos.
+B) quantos homens foram cadastrados.
+C) quantas mulheres tem menos de 20 anos. 
+'''
 
-a) Quantas pessoas tem mais de 18 anos.
-b) Quanto homens foram cadastrados.
-c) Quantas mulheres tem menos de 20 anos.'''
 pessoaMaior18 = totHomens = totMulher20 = 0
 while True:
     print('-' * 30)
