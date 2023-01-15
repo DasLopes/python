fad1178a112dd9f05d922da2c4c884062bb51bb4
daslopes <daslopes@outlook.com.br>
2023-01-15 09:23:32 -0300

    atualização das anotações da aula 13 for

--- aulas/aula13.py
+++ aulas/aula13.py
@@ -11,4 +11,43 @@ codigo em python
 for c in range(1,10):
     passo
 pega
+
+
+for c in range(0,6):
+    print(c)
+print('fim')
+
+for c in range(6,0,-1):
+    print(c)
+print('fim')
+
+for c in range(0,7,2):
+    print(c)
+print('fim')
+
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('fim')
+
+
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
+
+
+for c in range(0,3):
+    n = int(input('Digite um valor: '))
+print('fim')
+
 '''
+
+s = 0
+for c in range(0,4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório de todos os valores {}'.format(s))
