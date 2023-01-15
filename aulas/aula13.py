947cc45c961d37758fb99907c98314b5caab4844
daslopes <daslopes@outlook.com.br>
2023-01-15 09:02:17 -0300

    atualização das anotações

--- aulas/aula13.py
+++ aulas/aula13.py
@@ -1,1 +1,14 @@
-#Estrutura de repetição for
+'''Estrutura de repetição for
+
+laço c no intervalo(1,10) <- c é uma variavel de controle e pode ter qualquer nome
+
+laço c no intervalo(1,10)
+    passo
+pega
+
+codigo em python
+
+for c in range(1,10):
+    passo
+pega
+'''
